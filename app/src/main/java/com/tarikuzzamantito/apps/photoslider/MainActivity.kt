bc4c31e5cdce20a3c2cb2d71fef58cd775a90c90
tarikuzzamantito <tarikuzzaman1@gmail.com>
2025-03-29 20:12:09 +0600

    Initial commit

---
+++ app/src/main/java/com/tarikuzzamantito/apps/photoslider/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.tarikuzzamantito.apps.photoslider
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tarikuzzamantito.apps.photoslider.ui.theme.ComposePhotoSliderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposePhotoSliderTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageSlider(images: List<String>) {
+    val pagerState = rememberPagerState(
+        pageCount = { images.size },
+        initialPage = 0
+    )
+
+}
